7cf4b4c4f5b08b1711a7557d4c134643eae51d18
Araction <kvasilda97@mail.ru>
2019-09-26 13:28:53 +0300

    is it necessary to write a massage every time?

---
+++ lesson_1.py
@@ -0,0 +1,60 @@
+
+
+int_type_ex1 = 2019
+
+int_type_ex2 = 2031
+
+
+float_type_ex1 = 2.
+
+float_type_ex2 = 3.14
+
+
+bool_type_ex1 = True
+
+bool_type_ex2 = False
+
+
+list_type_ex1 = [4, 8, 16, 32]
+
+list_type_ex2 = [-2, -1, 0, 1, 2]
+
+
+tuple_type_ex1 = (1, 2, 3, 4)
+
+tuple_type_ex2 = (10, 20, 30, 40)
+
+
+dict_type_ex1 = {"x": 3, "y": 10}
+
+dict_type_ex2 = {"1-st": 99, "2-nd": 97, "3-rd": 95}
+
+
+print(f'Variable int_type_ex1: {int_type_ex1}')
+
+print(f'Variable int_type_ex2: {int_type_ex2}')
+
+
+print(f'Variable float_type_ex1: {float_type_ex1}')
+
+print(f'Variable float_type_ex2: {float_type_ex2}')
+
+
+print(f'Variable bool_type_ex1: {bool_type_ex1}')
+
+print(f'Variable bool_type_ex2: {bool_type_ex2}')
+
+
+print(f'Variable list_type_ex1: {list_type_ex1}')
+
+print(f'Variable list_type_ex2: {list_type_ex2}')
+
+
+print(f'Variable tuple_type_ex1: {tuple_type_ex1}')
+
+print(f'Variable tuple_type_ex2: {tuple_type_ex2}')
+
+
+print(f'Variable dict_type_ex1: {dict_type_ex1}')
+
+print(f'Variable dict_type_ex2: {dict_type_ex2}')
